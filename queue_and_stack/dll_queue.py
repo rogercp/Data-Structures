48c0b65f5bb0c096c9450801e5a5091a959e10f2
Roger Caballero <rogercp1234@yahoo.com>
2019-09-16 15:11:43 -0700

    changed queue to work of of import and finished stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,19 +6,19 @@ class Queue:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = []
+    self.storage = DoublyLinkedList()
 
   def enqueue(self, value):
-    self.storage.append(value)
+    self.storage.add_to_tail()
     print(f"enqueue")
     pass
   
   def dequeue(self):
-    item = self.storage.pop(0)
+    self.storage.remove_from_head()
     print(f"dequeued")
     return item
     pass
 
   def len(self):
-    return len(self.storage)
+    return self.storage.length
     pass
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,16 @@ class Stack:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    self.storage = DoublyLinkedList()
 
   def push(self, value):
+    self.storage.add_to_tail()
     pass
   
   def pop(self):
+    self.storage.remove_from_tail()
     pass
 
   def len(self):
+    return self.storage.length
     pass
