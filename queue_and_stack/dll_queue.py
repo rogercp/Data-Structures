a6d380ecc4a7bf63fa911a591c8f22a45f623e0c
Roger Caballero <rogercp1234@yahoo.com>
2019-09-16 15:01:06 -0700

    queued and dequeued methods working along with length

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,13 +6,19 @@ class Queue:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    self.storage = []
 
   def enqueue(self, value):
+    self.storage.append(value)
+    print(f"enqueue")
     pass
   
   def dequeue(self):
+    item = self.storage.pop(0)
+    print(f"dequeued")
+    return item
     pass
 
   def len(self):
+    return len(self.storage)
     pass
