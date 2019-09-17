9d261d7298b0b1a0981716bd867798589a80e64f
Roger Caballero <rogercp1234@yahoo.com>
2019-09-17 12:47:20 -0700

    added double linked list classes to cache

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.append('./doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 class Stack:
@@ -9,13 +9,13 @@ class Stack:
     self.storage = DoublyLinkedList()
 
   def push(self, value):
-    self.storage.add_to_tail()
-    pass
+    self.storage.add_to_tail(value)
+  
   
   def pop(self):
     self.storage.remove_from_tail()
-    pass
+  
 
   def len(self):
     return self.storage.length
-    pass
+  
